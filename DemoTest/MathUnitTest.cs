3d8a8f85dc19e2042a3a84190efad76658a51305
Ayman Gassara <a.gassara@root.sa>
2018-03-22 12:43:25 +0100

    Add project files.

---
+++ DemoTest/MathUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Demo.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoTest
+{
+    [TestClass]
+    public class MathUnitTest
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            // Arrange 
+
+            var vm = new MathViewModel();
+            vm.ValueA = 4;
+            vm.ValueB = 6;
+
+            //Act
+
+            vm.AddCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Result == 10, "vm.Result != 10 !");
+        }
+    }
+}
---
+++ DemoTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DemoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DemoTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c0c62c6-c357-4e0d-a0e4-9afe5ca42a01")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
